6b84189601f1232e763754594d748ec4c95c7775
chris24george <chris24george@gmail.com>
2023-04-20 13:40:17 -0700

    remove redundant user calling in controllers (it's already in middleware)

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -37,14 +37,13 @@ export const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error('Goal not found');
 	}
 
-	const user = await User.findById(req.user.id);
 	// check for user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error('User not found');
 	}
 	// make sure the logged in user matches the goal user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		res.status(401);
 		throw new Error('User not authorized');
 	}
@@ -65,15 +64,14 @@ export const deleteGoal = asyncHandler(async (req, res) => {
 		throw new Error('Goal not found');
 	}
 
-	const user = await User.findById(req.user.id);
 	// check for user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error('User not found');
 	}
 
 	// make sure the logged in user matches the goal user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		res.status(401);
 		throw new Error('User not authorized');
 	}
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,12 +70,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 // @route  GET /api/users/me
 // @access Private
 export const getMe = asyncHandler(async (req, res) => {
-	const { _id, name, email } = await User.findById(req.user.id);
-	res.status(200).json({
-		id: _id,
-		name,
-		email,
-	});
+	res.status(200).json(req.user);
 });
 
 // generate JWT
