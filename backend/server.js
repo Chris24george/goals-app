758ee67c842d329f788e92966b03605271098b63
chris24george <chris24george@gmail.com>
2023-04-24 09:58:39 -0700

    prepare for being deployed in prod

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import goalRouter from './routes/goalRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -9,6 +11,9 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 connectDB();
 
 const app = express();
@@ -17,6 +22,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', goalRouter);
 app.use('/api/users', userRouter);
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, req) => res.send('Please set node_env to production'));
+}
 
 app.use(errorHandler);
 
